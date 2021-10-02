94506ef8ffed98c9476dc26ff8ae5468062c746b
CHAN\jump_ <s45848229@gmail.com>
2021-10-02 16:38:13 +0900

    Initial commit

---
+++ src/main/java/com/web/chxkun/ChxkunApplication.java
@@ -0,0 +1,13 @@
+package com.web.chxkun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChxkunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChxkunApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/web/chxkun/ChxkunApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.chxkun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChxkunApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
