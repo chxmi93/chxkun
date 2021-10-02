51bf5af00109a060bb36796cc7603078b6674460
KimMyoungkun <6117smokz!>
2021-10-02 17:50:00 +0900

    Modify MainController

--- src/main/java/com/web/chxkun/controller/MainController.java
+++ src/main/java/com/web/chxkun/controller/MainController.java
@@ -1,6 +1,12 @@
 package com.web.chxkun.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class MainController {
-
-
+    @GetMapping("/")
+    public String mainView() {
+        return "content/main";
+    }
 }
